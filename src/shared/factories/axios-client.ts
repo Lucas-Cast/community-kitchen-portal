b09b0abf9200fdd93326089c1af61a95f66ff196
Lucas Castelano dos Santos <lucas.castelano@radixeng.com>
2025-06-28 15:49:00 -0300

    fix: fix middleware

--- src/shared/factories/axios-client.ts
+++ src/shared/factories/axios-client.ts
@@ -1,11 +1,11 @@
 import axios, { AxiosInstance } from 'axios'
 import Cookies from 'js-cookie'
 
-export function createAxiosClient(baseUrl: string): AxiosInstance {
+export function createAxiosClient(baseUrl: string, authToken?: string): AxiosInstance {
   const axiosInstance = axios.create({ baseURL: baseUrl })
 
   axiosInstance.interceptors.request.use(async config => {
-    const token = Cookies.get('authToken')
+    const token = authToken ?? Cookies.get('authToken')
     config.headers.Authorization = `Bearer ${token}`
     return config
   })
