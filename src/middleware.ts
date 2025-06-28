79de3d8b824343777968e859f716ee512f850a72
Lucas Castelano <100870230+Lucas-Cast@users.noreply.github.com>
2025-06-28 15:49:35 -0300

    fix: fix middleware (#7)
    Co-authored-by: Lucas Castelano dos Santos <lucas.castelano@radixeng.com>

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,14 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { userService } from './shared/services/user/user'
+import { environment } from '@/environment'
+import { TokenValidateResponse } from './shared/types/auth'
+import { createAxiosClient } from './shared/factories/axios-client'
+import { Routes } from './shared/enums/routes'
 
 export async function middleware(request: NextRequest) {
-  return await userService
-    .tokenValidate()
+  const authToken = request.cookies.get('authToken')?.value
+  const { post } = createAxiosClient(environment.userControllerApiUri, authToken)
+
+  return await post<TokenValidateResponse>(Routes.TOKEN_VALIDATE)
     .then(() => NextResponse.next())
     .catch(() => NextResponse.redirect(new URL('/auth', request.url)))
 }
