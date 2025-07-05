710b54343a18bb10d7469db058893f169f4810e2
Rafael Belmont <rafaelmagalhaes12323@gmail.com>
2025-07-05 08:04:22 -0300

    fixing eslint error

--- src/components/MenuTable/index.tsx
+++ src/components/MenuTable/index.tsx
@@ -1,14 +1,11 @@
 import { useMenus } from '@/shared/hooks/useMenus'
 import { DataTable } from '../DataTable'
 import { columns } from './columns'
-import { useOptimistic, useState } from 'react'
-import { Button } from '@/components/ui/button'
-import { Modal } from '../Modal'
+import { useOptimistic } from 'react'
 
 export default function MenuTable() {
   const menusData = useMenus()
   const [optimisticMenusData] = useOptimistic(menusData.data || [])
-  const [open, setOpen] = useState(false)
 
   return (
     <div className="container mx-auto py-10 space-y-4">
