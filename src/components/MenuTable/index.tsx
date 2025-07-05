1d5d2227571908037c705e0adba9d8ee9c65941d
Rafael Belmont <160741467+BelmontRafael@users.noreply.github.com>
2025-07-04 22:07:47 -0300

    Branch rafael belmont (#13)
    * feat: creating modal(dialog) component
    
    * feat: creating modal(dialog)  wrapper component
    
    * feat: adjusting modal paste
    
    * feat: adjusting modal component
    
    * fix: remove unused Dialog imports

--- src/components/MenuTable/index.tsx
+++ src/components/MenuTable/index.tsx
@@ -2,17 +2,9 @@ import { useMenus } from '@/shared/hooks/useMenus'
 import { DataTable } from '../DataTable'
 import { columns } from './columns'
 import { useOptimistic, useState } from 'react'
-import {
-  Dialog,
-  DialogTrigger,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogDescription,
-  DialogFooter,
-} from '@/components/ui/dialog'
 import { Button } from '@/components/ui/button'
-import { ModalWrapper } from '../ui/dialog-wrapper'
+
+import { Modal } from '../Modal/modal'
 
 export default function MenuTable() {
   const menusData = useMenus()
@@ -23,38 +15,18 @@ export default function MenuTable() {
   <div className="container mx-auto py-10 space-y-4">
 
     {/*Testando Modal*/}
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="default">Abrir Modal de Teste</Button>
-      </DialogTrigger>
-      <DialogContent size='sm' variant='form'>
-        <DialogHeader>
-          <DialogTitle>Modal de Teste</DialogTitle>
-          <DialogDescription>Isso é apenas um teste visual do modal.</DialogDescription>
-        </DialogHeader>
-        <div className="space-y-2">
-          <input className="w-full border px-4 py-2 rounded" placeholder="Campo de teste" />
-          <input className="w-full border px-4 py-2 rounded" placeholder="Outro campo" />
-        </div>
-        <DialogFooter>
-          <Button variant="ghost">Cancelar</Button>
-          <Button>Salvar</Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
-
-    {/*Testando ModalWrapper*/}
         <>
-      <Button onClick={() => setOpen(true)}>Abrir Modal de Teste 2</Button>
+      <Button onClick={() => setOpen(true)}>Abrir Modal de Teste</Button>
 
-      <ModalWrapper
+      <Modal
         isOpen={open}
         onClose={() => setOpen(false)}
         onConfirm={() => {
           console.log('Salvar enviado!')
           setOpen(false)
         }}
-        title="Modal de Teste com Wrapper"
+
+        title="Modal de Teste"
         description="Preencha os campos abaixo"
         size="lg"
         variant="alert"
@@ -69,7 +41,7 @@ export default function MenuTable() {
             placeholder="Outro campo"
           />
         </form>
-      </ModalWrapper>
+      </Modal>
     </>
 
     <div className="container mx-auto py-10">
