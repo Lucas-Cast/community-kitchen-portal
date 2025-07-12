fdf3ddae4d7ded4b3a5bfe6f782afa2e6784e23d
Gabriel <gsmainiere1901@gmail.com>
2025-07-12 00:55:41 -0300

    styles: changing visual erros of css

--- src/components/PageContainer/index.tsx
+++ src/components/PageContainer/index.tsx
@@ -1,6 +1,6 @@
 export default function PageContainer({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="flex flex-col items-center justify-start w-full h-screen">
+    <div className="flex flex-col items-center justify-start w-full min-dvh-screen">
       <div className="w-full max-w-screen-2xl px-6 py-4">{children}</div>
     </div>
   )
