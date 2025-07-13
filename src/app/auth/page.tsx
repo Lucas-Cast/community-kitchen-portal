5328cd1c05e4a6e31691d93700ddb75ccb33fb5e
Lucas Castelano <lucas.castelano27@gmail.com>
2025-07-13 20:36:07 -0300

    feat: signup

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -2,11 +2,14 @@
 
 import PageContainer from '@/components/PageContainer'
 import { SignIn } from '@/components/SignIn'
+import { SignUp } from '@/components/SignUp'
+import { useState } from 'react'
 
 export default function Page() {
+  const [isSignUp, setIsSignUp] = useState(false)
   return (
     <PageContainer>
-      <SignIn />
+      {isSignUp ? <SignUp setIsSignUp={setIsSignUp} /> : <SignIn setIsSignUp={setIsSignUp} />}
     </PageContainer>
   )
 }
