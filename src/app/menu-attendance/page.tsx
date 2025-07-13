c7a3e814b9e9baf1c738ace84b638d1453d2641d
Luiz Reis <reisluiz05@gmail.com>
2025-07-13 11:04:23 -0300

    feat: attendance route

---
+++ src/app/menu-attendance/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import MenuAttendanceTable from '@/components/MenuAttendance'
+import PageContainer from '@/components/PageContainer'
+
+export default function Page() {
+  return (
+    <PageContainer>
+      <MenuAttendanceTable />
+    </PageContainer>
+  )
+}
